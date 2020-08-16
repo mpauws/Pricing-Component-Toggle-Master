65cab952e39487625b05e17550403911e4ab9b5f
Marco Pauws <mcpauws@Marcos-MBP-3.fritz.box>
2020-08-16 11:32:32 +0200

    styling finish

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const toggleSwitch = document.getElementById("switch");
-const monthlyPricing = document.getElementsByClassName("monthly-pricing");
-const annualPricing = document.getElementsByClassName("annual-pricing");
+const monthlyPricing = document.getElementsByClassName(
+  "monthly-pricing-number"
+);
+const annualPricing = document.getElementsByClassName("annual-pricing-number");
 
 const displayToggle = (pricingCategory, displaySwitch) => {
-   [...pricingCategory].forEach((element) => {
-      element.style.display = displaySwitch;
-   });
+  [...pricingCategory].forEach((element) => {
+    element.style.display = displaySwitch;
+  });
 };
 
 toggleSwitch.addEventListener("change", () => {
-   if (toggleSwitch.checked) {
-      displayToggle(monthlyPricing, "none");
-      displayToggle(annualPricing, "block");
-   } else {
-      displayToggle(monthlyPricing, "block");
-      displayToggle(annualPricing, "none");
-   }
+  if (toggleSwitch.checked) {
+    displayToggle(monthlyPricing, "none");
+    displayToggle(annualPricing, "block");
+  } else {
+    displayToggle(monthlyPricing, "block");
+    displayToggle(annualPricing, "none");
+  }
 });
